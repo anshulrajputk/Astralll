d8ea6d7b4f237638570c666f8eba625fd7f7870a
Anshulrajput <121592478+anshulrajputk@users.noreply.github.com>
2025-08-13 15:14:00 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,33 +1,15 @@
-// utils/db.js
 const fs = require('fs');
-const path = require('path');
-
-// JSON file ka path (project ke andar utils folder ke andar)
-const dbFile = path.join(__dirname, 'welcomeData.json');
+const dbFile = './welcomeData.json';
 
 function loadDB() {
   if (!fs.existsSync(dbFile)) {
-    // Agar file nahi hai to empty object return karo
-    return {};
+    fs.writeFileSync(dbFile, JSON.stringify({}));
   }
-  try {
-    const data = fs.readFileSync(dbFile, 'utf8');
-    return JSON.parse(data);
-  } catch (error) {
-    console.error('DB load karte waqt error:', error);
-    return {};
-  }
+  return JSON.parse(fs.readFileSync(dbFile));
 }
 
 function saveDB(data) {
-  try {
-    fs.writeFileSync(dbFile, JSON.stringify(data, null, 2), 'utf8');
-  } catch (error) {
-    console.error('DB save karte waqt error:', error);
-  }
+  fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
 }
 
-module.exports = {
-  loadDB,
-  saveDB,
-};
+module.exports = { loadDB, saveDB };
